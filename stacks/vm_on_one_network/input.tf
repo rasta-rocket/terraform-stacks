14dc5ac36b01c2d55d5f8b13e9a1c83173bf928d
rasta-rocket <bruno.fernandos@yahoo.com>
2017-10-10 10:43:48 +0200

    Add vm_on_one_network stack
    * It implements one stack where all instances are on the same
      network/subnet.

---
+++ stacks/vm_on_one_network/input.tf
@@ -0,0 +1,18 @@
+# Compute
+variable instance_name {}
+variable image_name {}
+variable flavor_name {}
+variable key_pair {}
+
+# Network
+variable network_name {
+	default = "net_tf"
+}
+variable cidr {
+	default = "192.168.10.0/24"
+}
+
+# Count
+variable count {
+	default = "1"
+}
---
+++ stacks/vm_on_one_network/main.tf
@@ -0,0 +1,21 @@
+resource "openstack_networking_network_v2" "net" {
+	name = "${var.network_name}"
+}
+
+resource "openstack_networking_subnet_v2" "subnet" {
+	network_id = "${openstack_networking_network_v2.net.id}"
+	cidr = "${var.cidr}"
+}
+
+resource "openstack_compute_instance_v2" "instance" {
+	name = "${var.instance_name}_${count.index}"
+	image_name = "${var.image_name}"
+	flavor_name = "${var.flavor_name}"
+	key_pair = "${var.key_pair}"
+	security_groups = ["default"]
+
+	network {
+		name = "${openstack_networking_network_v2.net.name}"
+	}
+	count = "${var.count}"
+}
