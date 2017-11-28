7fca1c2ade75172836e0cc3569e6ed2705d95667
rasta-rocket <bruno.fernandos@yahoo.com>
2017-11-28 17:52:22 +0100

    Add apache_v01 stack

---
+++ stacks/apache_v01/main.tf
@@ -0,0 +1,39 @@
+module "backend" {
+	source = "../vm_with_floatingip"
+	instance_name = "${var.backend_name}"
+	image_name = "${var.image_name}"
+	flavor_name = "${var.flavor_name}"
+	key_pair = "${var.key_pair}"
+	ansible_group = "${var.backend_ansible_group}"
+	network_name = "${var.backend_net}"
+	cidr = "${var.backend_cidr}"
+	pool = "${var.pool}"
+        count = 1
+}
+
+module "bastion" {
+	source = "../bastion"
+	instance_name = "${var.bastion_name}"
+	image_name = "${var.image_name}"
+	flavor_name = "${var.flavor_name}"
+	key_pair = "${var.key_pair}"
+	ansible_group = "${var.bastion_ansible_group}"
+	network_name = "${var.bastion_net}"
+	cidr = "${var.bastion_cidr}"
+	pool = "${var.pool}"
+        count = 1
+}
+
+resource "openstack_networking_router_v2" "router" {
+  name = "${var.router_name}"
+}
+
+resource "openstack_networking_router_interface_v2" "router_interface_backend" {
+  router_id = "${openstack_networking_router_v2.router.id}"
+  subnet_id = "${module.backend.subnet_id[0]}"
+}
+
+resource "openstack_networking_router_interface_v2" "router_interface_bastion" {
+  router_id = "${openstack_networking_router_v2.router.id}"
+  subnet_id = "${module.bastion.subnet_id[0]}"
+}
---
+++ stacks/apache_v01/output.tf
@@ -0,0 +1,9 @@
+output "backend" {
+	value = "${module.backend.ip_address}"
+}
+output "bastion_ip" {
+	value = "${module.bastion.ip_address}"
+}
+output "bastion_fip" {
+        value = "${module.bastion.fip_address}"
+}
