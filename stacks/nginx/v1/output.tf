4dbd03261a2147f954739ef15a9d090b08aad8f1
rasta-rocket <bruno.fernandos@yahoo.com>
2018-01-05 00:05:42 +0100

    Format nginx_v1 stack

--- stacks/nginx/v1/output.tf
+++ stacks/nginx/v1/output.tf
@@ -1,12 +1,15 @@
 output "nginx_internal_ip" {
-	value = "${module.backend_v1.backend_ip}"
+  value = "${module.backend_v1.backend_ip}"
 }
+
 output "nginx_public_ip" {
-	value = "${module.backend_v1.backend_fip}"
+  value = "${module.backend_v1.backend_fip}"
 }
+
 output "bastion_internal_ip" {
-        value = "${module.backend_v1.bastion_ip}"
+  value = "${module.backend_v1.bastion_ip}"
 }
+
 output "bastion_public_ip" {
-        value = "${module.backend_v1.bastion_fip}"
+  value = "${module.backend_v1.bastion_fip}"
 }
