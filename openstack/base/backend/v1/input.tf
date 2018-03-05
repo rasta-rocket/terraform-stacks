9c2bc5b39a112d1314372de961e1d962810585da
rasta-rocket <bruno.fernandos@yahoo.com>
2018-03-05 22:19:22 +0100

    Update backend/v1 stack dependency to vm_with_floatingip
    * instead of bastion stack.
    * Format the configurations file.

--- openstack/base/backend/v1/input.tf
+++ openstack/base/backend/v1/input.tf
@@ -1,47 +1,54 @@
 # Default
-variable image_name {}
-variable flavor_name {}
-variable key_pair {}
+variable "image_name" {}
+
+variable "flavor_name" {}
+variable "key_pair" {}
 
 # Backend
-variable backend_name {
-	default = "backend"
+variable "backend_name" {
+  default = "backend"
 }
-variable backend_ansible_group {
-	default = "backend_group"
+
+variable "backend_ansible_group" {
+  default = "backend_group"
 }
 
 # Bastion
-variable bastion_name {
-	default = "bastion"
+variable "bastion_name" {
+  default = "bastion"
 }
-variable bastion_ansible_group {
-	default = "bastion_group"
+
+variable "bastion_ansible_group" {
+  default = "bastion_group"
 }
 
 # Network
-variable pool {
-	default = "public"
+variable "pool" {
+  default = "public"
 }
-variable backend_net {
-        default = "backend_net"
+
+variable "backend_net" {
+  default = "backend_net"
 }
-variable backend_cidr {
-        default = "192.168.10.0/24"
+
+variable "backend_cidr" {
+  default = "192.168.10.0/24"
 }
-variable bastion_net {
-        default = "bastion_net"
+
+variable "bastion_net" {
+  default = "bastion_net"
 }
-variable bastion_cidr {
-        default = "192.168.1.0/24"
+
+variable "bastion_cidr" {
+  default = "192.168.1.0/24"
 }
 
 # Router
-variable router_name {
-	default = "router"
+variable "router_name" {
+  default = "router"
 }
 
 # Count (number of backend)
-variable count {
-	default = "1"
+variable "count" {
+  default = "1"
 }
