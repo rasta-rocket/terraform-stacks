e1857f0d2529ce62116f37f8cb1c7589c0a05a1f
rasta-rocket <bruno.fernandos@yahoo.com>
2018-10-31 11:18:37 +0100

    cloudwatt/test_inter_tenant: improve the stack
    * Fmt the tf files.
    * Add the playbooks to install tools and to setup inter_tenant.
    * Split ansible groups to each project's backend.

--- cloudwatt/test_inter_tenant/input.tf
+++ cloudwatt/test_inter_tenant/input.tf
@@ -1,6 +1,9 @@
 # Provider
-variable "name_project_A" {}
+variable "auth_url" {}
 
+variable "region" {}
+
+variable "name_project_A" {}
 variable "user_project_A" {}
 variable "pwd_project_A" {}
 
@@ -19,37 +22,41 @@ variable "key_pair_project_A" {}
 variable "key_pair_project_B" {}
 
 # CIDR
-variable backend_cidr_project_A {
+variable "backend_cidr_project_A" {
   default = "192.168.20.0/24"
 }
 
-variable bastion_cidr_project_A {
+variable "bastion_cidr_project_A" {
   default = "192.168.2.0/24"
 }
 
-variable backend_cidr_project_B {
+variable "backend_cidr_project_B" {
   default = "192.168.30.0/24"
 }
 
-variable bastion_cidr_project_B {
+variable "bastion_cidr_project_B" {
   default = "192.168.3.0/24"
 }
 
 # Backend
-variable backend_name {
+variable "backend_name" {
   default = "backend_server"
 }
 
-variable backend_ansible_group {
-  default = "backend"
+variable "backend_ansible_group_project_A" {
+  default = "project_A"
+}
+
+variable "backend_ansible_group_project_B" {
+  default = "project_B"
 }
 
 # Bastion
-variable bastion_name {
+variable "bastion_name" {
   default = "bastion_server"
 }
 
-variable bastion_ansible_group {
+variable "bastion_ansible_group" {
   default = "bastion"
 }
 
@@ -59,24 +66,24 @@ variable "security_group_ssh_icmp_name" {
 }
 
 # Network
-variable external_net {
+variable "external_net" {
   default = "public"
 }
 
-variable backend_net {
+variable "backend_net" {
   default = "backend_net"
 }
 
-variable bastion_net {
+variable "bastion_net" {
   default = "bastion_net"
 }
 
 # Router
-variable gateway_name {
+variable "gateway_name" {
   default = "gateway"
 }
 
 # Count (number of backend)
-variable nb_backend {
+variable "nb_backend" {
   default = "1"
 }
